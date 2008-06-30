ef007d33f5b32bf1f6c27cad74d63aa41cd4dea5
f.canas.s <f.canas.s@6f8d09f0-d150-0410-92f3-417fd01af742>
2008-06-30 01:23:21 +0000

    Tidying up some code

--- src/m337.cpp
+++ src/m337.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include "m337_engine.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     m337_engine* eng = new m337_engine;
-	ifstream *archivo;
-	if(argc==2){
-		archivo=new ifstream(argv[1]);
-		if (!archivo->is_open()){
-			cout<<"Map not found"<<endl;
-			return -1;
-		}
 
-	}
-	else{
-		cerr<<"Uso %m337 mapa"<<endl;
-		return -1;
-	}
+    stringstream path;
+    path << "../data/maps/";
 
-    cout << eng->init_map(archivo) << endl;
-	
-	archivo->close();
+    ifstream* map_file;
+    if (argc==2) {
+        path << argv[1];
+    } else {
+        path << "2p_Test";
+    }
+
+    map_file = new ifstream(path.str().c_str(), ios_base::in);
+    if (!map_file->is_open()) {
+        cout << "Map not found" << endl;
+        return -1;
+    }        
+
+    cout << eng->init_map(map_file) << endl;
+    map_file->close();
     
-	cout << eng->add_unit("Char01", "../images/players/male01.png") << endl;
+    cout << eng->add_unit("Char01", "../images/players/male01.png") << endl;
     cout << eng->add_unit("Char02", "../images/players/female01.png") << endl;
 
     eng->init();
 
     eng->quit();
 
+    delete(map_file);
     delete(eng);
 
     return 0;
--- src/wengine/gamemap.h
+++ src/wengine/gamemap.h
@@ -8,21 +8,20 @@
 #include <fstream>
 
 using namespace std;
-//typedef 	vector<char> obj_list;
+
 class gamemap
 {
     public:
         gamemap();
         gamemap(int h,int w,ifstream *archivo);
         ~gamemap();
-		string get_path_to_sprite(int n);		
-		bool es_muro(int n);
+        string get_path_to_sprite(int n);		
+        bool es_muro(int n);
 
     protected:
         int get_tile_path_to_sprite(char symbol, string& path);
-		int h_, w_;
-		char *charmap_;
-
+        int h_, w_;
+        char *charmap_;
 };
 
 #endif
