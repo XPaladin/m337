a27eccdc654f90286a02a9cc2b829a27637bdbbd
f.canas.s <f.canas.s@6f8d09f0-d150-0410-92f3-417fd01af742>
2008-07-01 05:47:54 +0000

    Working on configuration nodes

--- src/wengine/util.h
+++ src/wengine/util.h
@@ -6,8 +6,23 @@
 typedef std::vector<char> char_vector;
 typedef std::vector<char_vector> char_matrix;
 
-namespace util {
-}
+inline int get_tile_index(int x, int y, int w);
+
+class coordinate
+{
+    public:
+        enum direction { UP, RIGHT, DOWN, LEFT };
+        int x;
+        int y;
+    public:
+        coordinate();
+        coordinate(int, int);
+
+        coordinate operator+(const coordinate&) const;
+        coordinate operator-(const coordinate&) const;
+        bool operator<(const coordinate&) const;
+        bool operator==(const coordinate&) const;
+};
 
 #endif
 
